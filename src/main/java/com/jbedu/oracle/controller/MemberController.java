e64b56253e1e3c1607515a3d39b60d9c60ca2ee2
luckyred <luckyred@gmail.com>
2025-01-06 11:49:54 +0900

    로그인 기능 추가

--- src/main/java/com/jbedu/oracle/controller/MemberController.java
+++ src/main/java/com/jbedu/oracle/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.jbedu.oracle.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +36,46 @@ public class MemberController {
 		
 		dao.joinMemberDao(mid, mpw, mname, memail, mage);
 		
+		model.addAttribute("memberName", mname);
+		
 		return "joinOk";
 	}
+	
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping(value = "/loginOk")
+	public String loginOk(HttpServletRequest request, Model model, HttpSession session) {
+		
+		String mid = request.getParameter("mid");
+		String mpw = request.getParameter("mpw");
+		
+		MemberDao dao = sqlSession.getMapper(MemberDao.class);
+		
+		int loginFlag = dao.loginDao(mid, mpw); // 반환값이 1이면 로그인 성공, 0이면 로그인 실패
+		
+		if(loginFlag == 1) { //참이면 로그인 성공
+			session.setAttribute("sessionId", mid);
+			model.addAttribute("loginId", mid);
+		} else {
+			
+			model.addAttribute("msg", "아이디 또는 비밀번호가 잘못 되었습니다. 다시 확인해주세요");
+			model.addAttribute("url", "login");
+			
+			return "alert";
+		}
+		
+		model.addAttribute("loginFlag", loginFlag); //로그인 성공 시 1이 전달, 실패 시 0이 전달
+		
+		return "loginOk";
+	}
+	
+	
+	
+	
+	
+	
 
 }
--- src/main/java/com/jbedu/oracle/dao/MemberDao.java
+++ src/main/java/com/jbedu/oracle/dao/MemberDao.java
@@ -3,5 +3,6 @@ package com.jbedu.oracle.dao;
 public interface MemberDao {
 	
 	public int joinMemberDao(String mid, String mpw, String mname, String memail, int mage); //회원 가입
+	public int loginDao(String mid, String mpw); //회원 로그인
 	
 }
